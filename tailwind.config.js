07bc88cb971d771d5c6a073b01b24b53b9a0794f
a-neey <2371429@hansung.ac.kr>
2026-03-09 09:51:57 +0900

    🎨 Chore: 기본 폴더 구조 및 alias 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  content: ["./src/app/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {},
