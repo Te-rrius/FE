8cd9179c7ac01165ad00f48d1f831364058fc44d
a-neey <2371429@hansung.ac.kr>
2026-03-12 23:46:59 +0900

    ✨ Feat: 로그인 관련 상태 추가

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -1,15 +1,46 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 interface AuthStore {
   token: string | null;
+  showLoginModal: boolean;
+  showKakaoWebView: boolean;
   login: () => void;
   logout: () => void;
+  returnPath: string;
+  openLoginModal: (returnPath?: string) => void;
+  closeLoginModal: () => void;
+  openKakaoWebView: () => void;
+  closeKakaoWebView: () => void;
 }
 
-const useAuthStore = create<AuthStore>((set) => ({
-  token: null,
-  login: () => set({ token: "tempToken" }),
-  logout: () => set({ token: null }),
-}));
+const useAuthStore = create<AuthStore>()(
+  persist(
+    (set) => ({
+      token: null,
+      showLoginModal: false,
+      showKakaoWebView: false,
+      returnPath: "/",
+      login: () =>
+        set({
+          token: "tempToken",
+          showKakaoWebView: false,
+          showLoginModal: false,
+        }),
+      logout: () => set({ token: null }),
+      openLoginModal: (returnPath = "/") =>
+        set({ showLoginModal: true, returnPath }),
+      closeLoginModal: () => set({ showLoginModal: false }),
+      openKakaoWebView: () => set({ showKakaoWebView: true }),
+      closeKakaoWebView: () => set({ showKakaoWebView: false }),
+    }),
+    {
+      name: "authToken",
+      storage: createJSONStorage(() => AsyncStorage),
+      partialize: (state) => ({ token: state.token }),
+    },
+  ),
+);
 
 export default useAuthStore;
