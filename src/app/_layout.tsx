c32ccd778025a1d41964c0664372d928b87892e6
a-neey <2371429@hansung.ac.kr>
2026-03-12 20:58:04 +0900

    🍱 Style: 프리텐다드 폰트 추가

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,6 +5,10 @@ export default function Layout() {
   useFonts({
     "KBLJump-EB-Condensed": require("../assets/fonts/KBLJump_EB_Condensed.ttf"),
     "KBLJump-EB-Extended": require("../assets/fonts/KBLJump_EB_Extended.ttf"),
+    Pretendard400: require("../assets/fonts/Pretendard-Regular.ttf"),
+    Pretendard500: require("../assets/fonts/Pretendard-Medium.ttf"),
+    Pretendard600: require("../assets/fonts/Pretendard-SemiBold.ttf"),
+    Pretendard700: require("../assets/fonts/Pretendard-Bold.ttf"),
   });
 
   return <Stack screenOptions={{ headerShown: false }} />;
