cc939f3da073ed5c13b5720203107729b152d1b1
a-neey <2371429@hansung.ac.kr>
2026-03-13 11:26:05 +0900

    🧪 Feat: 임시 로그인 버튼 추가

--- src/app/(public)/main.tsx
+++ src/app/(public)/main.tsx
@@ -1,11 +1,16 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
+import useAuthStore from "@/stores/authStore";
 
 export default function PublicRoute() {
+  const { openLoginModal } = useAuthStore();
+
   return (
     <SafeAreaView>
       <View>
-        <Text>main</Text>
+        <Pressable onPress={() => openLoginModal("/main")}>
+          <Text>로그인</Text>
+        </Pressable>
       </View>
     </SafeAreaView>
   );
