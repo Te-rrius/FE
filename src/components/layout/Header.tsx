2fb9bfbcb1d6fa11c88e93474c74699eede00803
a-neey <2371429@hansung.ac.kr>
2026-03-17 18:10:08 +0900

    ✨ Feat: 로그인 여부 아이콘 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,71 +1,78 @@
-import { Pressable, StyleSheet, Text, View } from "react-native"
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
-import MainLogoIcon from "@/assets/images/mainLogoIcon.svg";
-import TennisIcon from "@/assets/images/header/tennisIcon.svg";
-import { hp, wp } from "@/utils/dimension";
-import useAuthStore from "@/store/authStore";
+import MainLogoIcon from '@/assets/images/mainLogoIcon.svg';
+import TennisIcon from '@/assets/images/header/tennisIcon.svg';
+import AuthIcon from '@/assets/images/header/authIcon.svg';
+import { hp, wp } from '@/utils/dimension';
+import useAuthStore from '@/store/authStore';
 
 const Header = () => {
-  const { openLoginModal } = useAuthStore();
+  const { openLoginModal, token } = useAuthStore();
+  const isLogin = !!token;
 
-    return (
-        <View style={styles.container}>
-            <View style={styles.logo}>
-                <MainLogoIcon />
-                <View style={styles.tennis}>
-                    <TennisIcon />
-                    <Text style={styles.tennisText}>테니스</Text>
-                </View>
-            </View>
-            <Pressable style={styles.authButton} onPress ={() => openLoginModal()}>
-                <Text style={styles.authText}>로그인 / 회원가입</Text>
-            </Pressable>
+  return (
+    <View style={styles.container}>
+      <View style={styles.logo}>
+        <MainLogoIcon />
+        <View style={styles.tennis}>
+          <TennisIcon />
+          <Text style={styles.tennisText}>테니스</Text>
         </View>
-    )
-}
+      </View>
+
+      {isLogin ? (
+        <AuthIcon />
+      ) : (
+        <Pressable style={styles.authButton} onPress={() => openLoginModal()}>
+          <Text style={styles.authText}>로그인 / 회원가입</Text>
+        </Pressable>
+      )}
+    </View>
+  );
+};
 
 export default Header;
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        paddingHorizontal: wp(20),
-        backgroundColor: "#FCFCFC"
-    },
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: wp(20),
+    backgroundColor: '#FCFCFC',
+  },
 
-    logo: {
-        flexDirection: "row",
-        alignItems: "center",
-        gap: wp(12)
-    },
+  logo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: wp(12),
+  },
 
-    tennis: {
-        flexDirection: "row",
-        alignItems: "center",
-        backgroundColor: "#2BAE66",
-        borderRadius: 4,
-        paddingHorizontal: wp(6),
-        paddingVertical: hp(4),
-        gap: wp(6),
-    },
+  tennis: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#2BAE66',
+    borderRadius: 4,
+    paddingHorizontal: wp(6),
+    paddingVertical: hp(4),
+    gap: wp(6),
+  },
 
-    tennisText: {
-        color: "#FCF6F5",
-        fontFamily: "Pretendard700"
-    },
+  tennisText: {
+    color: '#FCF6F5',
+    fontFamily: 'Pretendard700',
+  },
 
-    authButton: {
-        backgroundColor: "#F5F5F5",
-        borderColor: "#E8E8E8",
-        borderWidth: 1,
-        paddingHorizontal: wp(14),
-        paddingVertical: hp(6)
-    },
+  authButton: {
+    backgroundColor: '#F5F5F5',
+    borderColor: '#E8E8E8',
+    borderWidth: 1,
+    paddingHorizontal: wp(14),
+    paddingVertical: hp(6),
+  },
 
-    authText: {
-        color: "#303030",
-        fontSize: 14,
-        fontFamily: "Pretendard600"
-    }
-})
+  authText: {
+    color: '#303030',
+    fontSize: 14,
+    fontFamily: 'Pretendard600',
+  },
+});
