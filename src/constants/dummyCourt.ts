05172958f22c7b99ab5a258a2e16d992e727eec3
a-neey <2371429@hansung.ac.kr>
2026-03-22 03:56:28 +0900

    🍱 Asset: 임시 이미지 추가

--- src/constants/dummyCourt.ts
+++ src/constants/dummyCourt.ts
@@ -1,3 +1,5 @@
+import courtImg from '@/assets/images/mainLogoIcon.svg';
+
 export interface CourtDto {
   courtId: number;
   name: string;
@@ -15,12 +17,14 @@ export const DUMMY_COURTS: CourtDto[] = [
     courtId: 1,
     name: '천마 풋살파크',
     location: '서울시 송파구 성내천로29길 28',
-    image: '',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6OqB5wGlgjtUIai09yKi_Z9gCa0roO1fjAQ&s',
   },
   {
     courtId: 2,
     name: '데일리그라운드 일산 마두점',
     location: '경기도 고양시 일산동구 중앙로 1160',
-    image: '',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6OqB5wGlgjtUIai09yKi_Z9gCa0roO1fjAQ&s',
   },
 ];
