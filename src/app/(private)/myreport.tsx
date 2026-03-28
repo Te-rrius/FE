a4e1ec6aafef29639327930c3d8199575756719f
a-neey <2371429@hansung.ac.kr>
2026-03-29 03:02:59 +0900

    ✏️ Fix: 이름 중복 수정

--- src/app/(private)/myreport.tsx
+++ src/app/(private)/myreport.tsx
@@ -2,7 +2,7 @@ import MyReportScreen from '@/screens/accout/report/MyReportScreen';
 import { StyleSheet, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function MyPage() {
+export default function MyReport() {
   return (
     <View style={styles.mypageContainer}>
       <SafeAreaView>
