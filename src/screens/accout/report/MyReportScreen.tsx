260736e58bcf39ed7a7709f3b21bf7636d9c874e
a-neey <2371429@hansung.ac.kr>
2026-03-29 02:37:13 +0900

    ✨ Feat: ReportCard 구현

--- src/screens/accout/report/MyReportScreen.tsx
+++ src/screens/accout/report/MyReportScreen.tsx
@@ -3,6 +3,14 @@ import PageHeader from '@/components/layout/PageHeader';
 import { hp, wp } from '@/utils/dimension';
 import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ReportCard from './components/ReportCard';
+
+const dummyReports = [
+  { id: 1, date: '2026. 06. 05.' },
+  { id: 2, date: '2026. 05. 20.' },
+  { id: 3, date: '2026. 05. 10.' },
+  { id: 4, date: '2026. 04. 30.' },
+];
 
 const MyReportScreen = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -33,6 +41,11 @@ const MyReportScreen = () => {
               setIsDropdownOpen(false);
             }}
           />
+          <View style={styles.listWrapper}>
+            {dummyReports.map((report) => (
+              <ReportCard key={report.id} date={report.date} />
+            ))}
+          </View>
         </View>
       </View>
     </View>
@@ -73,5 +86,12 @@ const styles = StyleSheet.create({
     fontSize: wp(14),
     lineHeight: hp(20),
     letterSpacing: wp(-0.35),
+  },
+
+  listWrapper: {
+    paddingTop: hp(11),
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    gap: wp(4),
   },
 });
---
+++ src/screens/accout/report/components/ReportCard.tsx
@@ -0,0 +1,83 @@
+import { hp, wp } from '@/utils/dimension';
+import { StyleSheet, Text, View } from 'react-native';
+import CardGradient from './CardGradient';
+import TennisCardIcon from '@/assets/images/account/tennisCardIcon.svg';
+
+type ReportCardProps = {
+  date: string;
+};
+
+const ReportCard = ({ date }: ReportCardProps) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.cardHeader}>
+        <View style={styles.gradientWrapper}>
+          <CardGradient />
+          <CardGradient width={wp(53)} />
+          <CardGradient width={wp(70)} />
+        </View>
+        <View style={styles.tennisIcon}>
+          <TennisCardIcon />
+        </View>
+      </View>
+      <View style={styles.reportText}>
+        <Text style={styles.dateText}>{date}</Text>
+        <Text style={styles.titleText}>{`테니스\n분석 리포트`}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default ReportCard;
+
+const styles = StyleSheet.create({
+  container: {
+    width: '49%',
+    aspectRatio: 1,
+    paddingHorizontal: wp(12),
+    paddingVertical: hp(12),
+    borderWidth: 1,
+    borderColor: '#E8E8E8',
+    borderRadius: 18,
+    gap: hp(4),
+  },
+
+  cardHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  gradientWrapper: {
+    gap: hp(6),
+  },
+
+  tennisIcon: {
+    width: wp(44),
+    height: hp(44),
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#4048F7',
+    borderRadius: 18,
+  },
+
+  reportText: {
+    gap: hp(4),
+  },
+
+  dateText: {
+    color: '#666666',
+    fontFamily: 'Pretendard400',
+    fontSize: wp(12),
+    lineHeight: hp(17),
+    letterSpacing: wp(-0.3),
+  },
+
+  titleText: {
+    color: '#333333',
+    fontFamily: 'KBLJump-EB-Extended',
+    fontSize: wp(16),
+    lineHeight: hp(23),
+    letterSpacing: wp(0.32),
+  },
+});
