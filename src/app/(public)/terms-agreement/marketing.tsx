1abf7b9c676559375c3fec035a4dcc2cc6be6515
a-neey <2371429@hansung.ac.kr>
2026-03-30 10:13:42 +0900

    🚚 Route: 약관 동의 퍼블릭으로 이동

---
+++ src/app/(public)/terms-agreement/marketing.tsx
@@ -0,0 +1,12 @@
+import ScreenContainer from '@/components/layout/ScreenContainer';
+import MarketingAgreeScreen from '@/screens/accout/setting/terms-agreement/MarketingAgreeScreen';
+
+const MarketingDetail = () => {
+  return (
+    <ScreenContainer>
+      <MarketingAgreeScreen />
+    </ScreenContainer>
+  );
+};
+
+export default MarketingDetail;
