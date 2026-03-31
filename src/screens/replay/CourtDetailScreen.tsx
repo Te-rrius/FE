ac5e61bbf0b78528aa17b6839e5ce2029357546a
a-neey <2371429@hansung.ac.kr>
2026-03-31 18:56:12 +0900

    ✨ Feat: 분석 리포트 신청 구현

--- src/screens/replay/CourtDetailScreen.tsx
+++ src/screens/replay/CourtDetailScreen.tsx
@@ -5,15 +5,20 @@ import { DUMMY_COURTS } from '@/constants/dummyCourt';
 import { hp, wp } from '@/utils/dimension';
 import { useState } from 'react';
 import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
-import ReportDownBanner from '@/assets/images/banner/reportDownBanner.svg';
 import DetailToggle from '@/components/common/DetailToggle';
 import StepScroll from '@/components/common/StepScroll';
 
 import DownStep1Icon from '@/assets/images/common/downStep1Icon.svg';
 import DownStep2Icon from '@/assets/images/common/downStep2Icon.svg';
 import DownStep3Icon from '@/assets/images/common/downStep3Icon.svg';
+import ReportDownBanner from '@/assets/images/banner/reportDownBanner.svg';
+
+import RequestStep1Icon from '@/assets/images/common/requestStep1Icon.svg';
+import RequestStep2Icon from '@/assets/images/common/requestStep2Icon.svg';
+import ReportRequestBanner from '@/assets/images/banner/reportRequestBanner.svg';
 
 const DOWN_STEPS = [DownStep1Icon, DownStep2Icon, DownStep3Icon];
+const REQUEST_STEPS = [RequestStep1Icon, RequestStep2Icon];
 
 interface CourtDetailProps {
   courtId: string | string[];
@@ -38,30 +43,34 @@ const CourtDetailScreen = ({ courtId }: CourtDetailProps) => {
         </View>
         <Tab
           tabs={[
-            {
-              title: '분석 리포트 다운',
-              handler: () => {
-                setSelectedTab('분석 리포트 다운');
-              },
-            },
-            {
-              title: '분석 리포트 신청',
-              handler: () => {
-                setSelectedTab('분석 리포트 신청');
-              },
-            },
+            { title: '분석 리포트 다운', handler: () => setSelectedTab('분석 리포트 다운') },
+            { title: '분석 리포트 신청', handler: () => setSelectedTab('분석 리포트 신청') },
           ]}
           activeTab={selectedTab}
         />
       </View>
-      <DetailToggle title="분석 리포트 받아보기">
-        <StepScroll images={DOWN_STEPS} />
-      </DetailToggle>
 
-      <Divider />
-      <View style={styles.bannerWrapper}>
-        <ReportDownBanner />
-      </View>
+      {selectedTab === '분석 리포트 다운' ? (
+        <>
+          <DetailToggle title="분석 리포트 받아보기">
+            <StepScroll images={DOWN_STEPS} />
+          </DetailToggle>
+          <Divider />
+          <View style={styles.bannerWrapper}>
+            <ReportDownBanner />
+          </View>
+        </>
+      ) : (
+        <>
+          <DetailToggle title="분석 리포트 신청">
+            <StepScroll images={REQUEST_STEPS} />
+          </DetailToggle>
+          <Divider />
+          <View style={styles.bannerWrapper}>
+            <ReportRequestBanner />
+          </View>
+        </>
+      )}
     </ScrollView>
   );
 };
