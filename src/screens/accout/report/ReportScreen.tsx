b1f334666965d9a98e7bc3e83c7f88c08ed619ed
a-neey <2371429@hansung.ac.kr>
2026-04-10 13:28:56 +0900

    ✨ Feat: 리포트 헤더 구현

--- src/screens/accout/report/ReportScreen.tsx
+++ src/screens/accout/report/ReportScreen.tsx
@@ -1,9 +1,47 @@
+import PageHeader from '@/components/layout/PageHeader';
+import { StyleSheet, Text, View } from 'react-native';
+
+import ReportDownloadIcon from '@/assets/images/report/reportDownloadIcon.svg';
+import { hp, wp } from '@/utils/dimension';
+
 type ReportScreenProps = {
   reportId: string | string[];
 };
 
 const ReportScreen = ({ reportId }: ReportScreenProps) => {
-  return <></>;
+  return (
+    <>
+      <PageHeader
+        rightContent={
+          <View style={styles.headerDownContainer}>
+            <Text style={styles.headerDownText}>리포트 다운</Text>
+            <ReportDownloadIcon />
+          </View>
+        }
+      />
+    </>
+  );
 };
 
 export default ReportScreen;
+
+const styles = StyleSheet.create({
+  headerDownContainer: {
+    paddingHorizontal: wp(10),
+    paddingVertical: hp(6),
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: wp(4),
+    borderWidth: 1,
+    borderColor: '#E8E8E8',
+    borderRadius: 100,
+  },
+
+  headerDownText: {
+    color: '#999999',
+    fontFamily: 'Pretendard600',
+    fontSize: wp(14),
+    lineHeight: hp(20),
+    letterSpacing: wp(-0.3),
+  },
+});
