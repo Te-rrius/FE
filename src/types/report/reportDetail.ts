2f1e2d3a41c4b9945caa20434830b3a38810f471
a-neey <2371429@hansung.ac.kr>
2026-05-20 15:47:35 +0900

    🏷️ Feat: 리포트 상세 조회 타입 정의

---
+++ src/types/report/reportDetail.ts
@@ -0,0 +1,31 @@
+// 리포트 상세 조회
+export type PlayerTarget = 'PLAYER_ONE' | 'PLAYER_TWO';
+
+export interface MaterialResponse {
+  materialType: 'MOTION' | 'WINNING_SHOT' | 'WORST_SHOT';
+  materialTypeName: string;
+  videoUrl: string;
+}
+
+export interface ReportDetailResponse {
+  reportId: number;
+  matchVideoId: number;
+  matchDate: string;
+  target: PlayerTarget;
+  targetName: string;
+  shotType: string;
+  shotTypeName: string;
+  maxSpeed: number;
+  shoulderRotationAngle: number;
+  spineRotationAngle: number;
+  waistRotationAngle: number;
+  averageRallyCount: number;
+  maxRallyCount: number;
+  minRallyCount: number;
+  totalShotCount: number;
+  improvementPoint: string;
+  firstServeSuccessRate: number;
+  secondServeSuccessRate: number;
+  firstServeRate: number;
+  materials: MaterialResponse[];
+}
