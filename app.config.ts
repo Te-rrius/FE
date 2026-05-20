8a39ce3fc555bd8b10a41ca0b098437548473b37
a-neey <2371429@hansung.ac.kr>
2026-05-20 11:53:04 +0900

    🐛 Fix: 이미지 경로 https 빌드 시 설정

--- app.config.ts
+++ app.config.ts
@@ -1,56 +1,59 @@
 export default {
   expo: {
-    name: "Terrius",
-    slug: "terrius",
-    scheme: "terrius",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./src/assets/icon.png",
-    userInterfaceStyle: "light",
+    name: 'Terrius',
+    slug: 'terrius',
+    scheme: 'terrius',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './src/assets/icon.png',
+    userInterfaceStyle: 'light',
 
     splash: {
-      image: "./src/assets/splash-icon.png",
-      resizeMode: "contain",
-      backgroundColor: "#ffffff",
+      image: './src/assets/splash-icon.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
     },
 
     ios: {
       supportsTablet: true,
-      bundleIdentifier: "com.terrius.app",
+      bundleIdentifier: 'com.terrius.app',
+      infoPlist: {
+        NSAppTransportSecurity: {
+          NSAllowsArbitraryLoads: true,
+        },
+      },
     },
 
     android: {
-      package: "com.terrius.app",
+      package: 'com.terrius.app',
       predictiveBackGestureEnabled: false,
       adaptiveIcon: {
-        backgroundColor: "#E6F4FE",
-        foregroundImage: "./src/assets/android-icon-foreground.png",
-        backgroundImage: "./src/assets/android-icon-background.png",
-        monochromeImage: "./src/assets/android-icon-monochrome.png",
+        backgroundColor: '#E6F4FE',
+        foregroundImage: './src/assets/android-icon-foreground.png',
+        backgroundImage: './src/assets/android-icon-background.png',
+        monochromeImage: './src/assets/android-icon-monochrome.png',
       },
     },
 
     web: {
-      favicon: "./src/assets/favicon.png",
+      favicon: './src/assets/favicon.png',
     },
 
     plugins: [
-      "expo-router",
-      "expo-font",
+      'expo-router',
+      'expo-font',
 
       [
-        "expo-build-properties",
+        'expo-build-properties',
         {
           android: {
-            extraMavenRepos: [
-              "https://devrepo.kakao.com/nexus/content/groups/public/",
-            ],
+            extraMavenRepos: ['https://devrepo.kakao.com/nexus/content/groups/public/'],
           },
         },
       ],
 
       [
-        "@react-native-kakao/core",
+        '@react-native-kakao/core',
         {
           nativeAppKey: process.env.EXPO_PUBLIC_KAKAO_NATIVE_APP_KEY,
           android: {
