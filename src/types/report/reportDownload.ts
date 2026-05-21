be2f85bf10bbf97009e201d0b4e8c4169cc60e31
a-neey <2371429@hansung.ac.kr>
2026-05-22 00:21:24 +0900

    🏷️ Feat: 리포트 시간대 조회 타입 정의

--- src/types/report/reportDownload.ts
+++ src/types/report/reportDownload.ts
@@ -1,5 +1,15 @@
+// 리포트 존재 여부
 export interface ReportDateResponse {
   date: string;
   dayOfWeek: string;
   hasReport: boolean;
+}
+
+// 리포트 시간대 조회
+export interface ReportTimeResponse {
+  matchVideoId: number;
+  startTime: string;
+  endTime: string;
+  matchType: 'SINGLES' | 'DOUBLES';
+  reportRequested: boolean;
 }
