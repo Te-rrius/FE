a211f6f0a1547e70a42429a70f84fec3b922d50a
a-neey <2371429@hansung.ac.kr>
2026-06-02 01:38:53 +0900

    💄 Style: 리포트 동작 영상 비율 변경

--- src/screens/accout/report/components/PoseAnalysis.tsx
+++ src/screens/accout/report/components/PoseAnalysis.tsx
@@ -255,7 +255,7 @@ const styles = StyleSheet.create({
 
   videoContainer: {
     width: wp(350),
-    aspectRatio: 9 / 16,
+    aspectRatio: 4 / 3,
     backgroundColor: '#000',
     borderRadius: 12,
     overflow: 'hidden',
